2b19f42ebd7be99627db0d2ef951fc6a2426a7c9
Lukas Frei <lukas.frei@zuehlke.cim>
2019-10-26 13:42:46 +0200

    fix formatting

--- src/app/flight-detail.component.ts
+++ src/app/flight-detail.component.ts
@@ -6,28 +6,28 @@ import { Component, Input } from '@angular/core';
     <div class="card">
         <div class="card-body">
             <div class="row">
-                <div class="col-md-2">Date:</div>
-                <div class="col-md-2">{{flight.date | date: 'dd.MM.yyyy'}}</div>
+                <div class="col-md-3">Date:</div>
+                <div class="col-md-3">{{flight.date | date: 'dd.MM.yyyy'}}</div>
             </div>
             <div class="row">
-                <div class="col-md-2">Takeoff:</div>
-                <div class="col-md-2">{{flight.takeoff}}</div>
+                <div class="col-md-3">Takeoff:</div>
+                <div class="col-md-3">{{flight.takeoff}}</div>
             </div>
             <div class="row">
-                <div class="col-md-2">Landing:</div>
-                <div class="col-md-2">{{flight.landing}}</div>
+                <div class="col-md-3">Landing:</div>
+                <div class="col-md-3">{{flight.landing}}</div>
             </div>
             <div class="row">
-                <div class="col-md-2">Duration:</div>
-                <div class="col-md-2">{{flight.duration}} min</div>
+                <div class="col-md-3">Duration:</div>
+                <div class="col-md-3">{{flight.duration}} min</div>
             </div>
             <div class="row">
-                <div class="col-md-2">Description:</div>
-                <div class="col-md-8">{{flight.description}}</div>
+                <div class="col-md-3">Description:</div>
+                <div class="col-md-7">{{flight.description}}</div>
             </div>
             <div class="row">
-                <div class="col-md-2">Rating:</div>
-                <div class="col-md-2">
+                <div class="col-md-3">Rating:</div>
+                <div class="col-md-3">
                     <ngb-rating [(rate)]="flight.rating" [readonly]="true" max="5"></ngb-rating>
                 </div>
             </div>
